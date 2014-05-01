3e87d85259e9863077c435aaa8acb740e4ea5478
Andrew Krause <a.c.krause@gmail.com>
2014-04-30 20:28:12 -0700

    Initial view for song queue, based on view of song library

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,10 +1,24 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName: 'table',
+
   initialize: function() {
+    this.render();
+
+    // re-render when song is added or removed from queue
+    this.collection.on('change', function() { this.render(); });
   },
 
   render: function() {
+    this.$el.children().detach();
+
+    this.$el.html('<th>Queue</th>').append(
+      this.collection.map(function(song) {
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
+
     return this.$el;
   }
 
